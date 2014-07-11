aed1f12553f7edd506160173795ab12319694021
tag1 <rlthompsonjr@gmail.com>
2014-07-11 11:31:21 -0400

    Draft #2
    Added instruction javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
-  $(".listul").sortable();
-  
+	$('#listul').sortable({ axis : "y"});
+	handle: '.movegrid'
+
+	$("#instructobutton").click (function () {
+	$(this).hide();
+	$("#destructobutton").show();
+	$(".instructions").fadeIn(1000);
+	});
+
+	$("#destructobutton").click (function () {
+	$(this).hide();
+	$("#instructobutton").show();
+	$(".instructions").fadeOut(10);
+	});
+
+	$(".enterbox").keypress(13, (function () {
+	$("listul").prepend();
+	}))
 });
