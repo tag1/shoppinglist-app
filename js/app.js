828d470dc447f9dea38187ff7b1909f14bf776ff
tag1 <rlthompsonjr@gmail.com>
2014-07-12 03:58:17 -0400

    Draft #3
    Added input functionality, changed HTML5 list sorting to jquery UI list
    sorting.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
-$( document ).ready(function() {
-	$('#listul').sortable({ axis : "y"});
-	handle: '.movegrid'
+	$(document).ready(function() {
 
-	$("#instructobutton").click (function () {
+	$("#sortable").sortable();
+
+	$("#instructobutton").click(function () {
 	$(this).hide();
 	$("#destructobutton").show();
 	$(".instructions").fadeIn(1000);
 	});
 
-	$("#destructobutton").click (function () {
+	$("#destructobutton").click(function () {
 	$(this).hide();
 	$("#instructobutton").show();
-	$(".instructions").fadeOut(10);
+	$(".instructions").fadeOut(1000);
 	});
 
-	$(".enterbox").keypress(13, (function () {
-	$("listul").prepend();
-	}))
+	$("#redX").click (function () {
+	$("listitem").remove();
+	});
+
+	$("#enterbutton").click(function () {
+	var addItem = $("#enterbox").val();
+	$("#sortable").prepend('<li class="listitem"><button class="movegrid"><span class="glyphicon glyphicon-unchecked"></span></button><p>'  +  addItem + '</p><button class="redX"><span class="glyphicon glyphicon-remove"></span></button></li>')
+	});
+
+	$(".movegrid").click(function () {
+	$(this).siblings("li").fadeTo(400, "fast")
+	});
 });
